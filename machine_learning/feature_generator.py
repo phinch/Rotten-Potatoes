0e2ada2766eafb3414415d9cf18378cab2969ab2
Gary Chien <garychien1@gmail.com>
2016-03-21 23:56:43 -0400

    ML feature generation code added

---
+++ machine_learning/feature_generator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import csv
+import sys
+import glob
+import os
+
+def main():
+
+	csv_filenames = []
+	score_reviews = []
+	
+	for file in glob.glob(data_path + '/*.csv'):
+		csv_filenames.append(os.path.basename(file))
+
+	for csv_filename in csv_filenames:
+		with open(data_path + '/' + csv_filename) as reviews:
+			reader = csv.DictReader(reviews, delimiter='|')
+			for row in reader:
+				text = row['Review Text']
+				score = int(row['Review Score'])
+				label = 1 # positive label
+				if score < 3:
+					label = 0 # negative label
+				score_reviews.append((label, text))
+	
+	with open('features.csv', 'wb') as output:
+		writer = csv.writer(output, delimiter='|')
+		writer.writerow(['positivity', 'text'])
+		for tup in score_reviews:
+			writer.writerow([tup[0], tup[1]])
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print 'Usage: python review_classifier.py data_path'
+		sys.exit()
+	data_path = sys.argv[1]
+	main()
