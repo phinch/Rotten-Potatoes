3d37be9f33c132dbd17c1d93ffe1c38ebda087b2
Philip Hinch <phinch@brown.edu>
2016-04-19 00:37:21 -0400

    Foursquare/Yelp Genre Bias

--- Philip's WIP Stuff/genre_bias.py
+++ Philip's WIP Stuff/genre_bias.py
@@ -1,103 +1,158 @@
 """
 Measures bias between review sites organized by genre. 
 Genres have been manually created by observing the data.
-INITIAL DISPLAY:
-    American
-    Asian
-    European
-    Dessert
-    Cafe
-    Bar
 
-OTHER CHOICES:
-    Pizza
-    Sandwiches+Deli
-    Non-American
-    Non-White
-    Food Trucks
-    Brunch
-    Mediterannean
-    Latin American
-    French
-    Italian
-    Korean 
-    Chinese
-    Thai
-    Japanese
 """
 from lxml import html
 import requests
 import math
 import csv
 import json
+categories = {
+    "Sushi Bars": ["Japanese", "Asian", "Non-American", "Non-White"],
+    "Japanese": ["Japanese", "Asian", "Non-American", "Non-White"],
+    "Cantonese": ["Chinese", "Asian", "Non-American", "Non-White"],
+    "Chinese": ["Chinese", "Asian", "Non-American", "Non-White"],
+    "Thai": ["Thai", "Asian", "Non-American", "Non-White"],
+    "Indian": ["Asian", "Non-American", "Non-White"],
+    "Korean": ["Korean", "Asian", "Non-American", "Non-White"],
+    "Pakistani": ["Asian", "Non-American", "Non-White"],
+    "Mongolian": ["Asian", "Non-American", "Non-White"],
+    "Asian Fusion": ["Asian", "Non-American", "Non-White"],
+    "Vietnamese": ["Asian", "Non-American", "Non-White"],
+    "Cambodian": ["Asian", "Non-American", "Non-White"],
 
-def otread(name):
-    ot_reader = csv.reader(open(name, 'rb'), delimiter = "|")
-    next(ot_reader, None)
-    stars = 0
-    count = 0
+    "French": ["French", "European", "Non-American"],
+    "Creperies": ["French", "European", "Non-American"],
+    "Italian": ["Italian", "European", "Non-American"],
+    "Tapas/Small Plates": ["European", "Non-American"],
+    "Tapas Bars": ["European", "Non-American"],
+    "Irish": ["European", "Non-American"],
+    "British": ["European", "Non-American"],
+    
+    "Bar": ["Bars"],
+    "Dive Bar": ["Bars"],
+    "Cocktail Bar": ["Bars"],
+    "Dive Bars": ["Bars"],
+    "Pub": ["Bars"],
+    "Beer Bar": ["Bars"],
+    "Bars": ["Bars"],
+    "Pubs": ["Bars"],
+    "Gastropubs": ["Bars"],
+    "Wine Bars": ["Bars"],
 
-    for line in ot_reader: #Review Title|Review Date|Review Score|Review Text
-        count += 1
-        stars += float(line[2])
+    "Coffee & Tea": ["Cafes"],
+    "Bubble Tea": ["Cafes"],
+    "Cafes": ["Cafes"],
+    "Cafe": ["Cafes"],
+    "Juice Bar": ["Cafes"],
 
-    return stars/count
+    "Steakhouses": ["American"],
+    "American (New)": ["American"],
+    "Breakfast and Brunch": ["Brunch", "American"],
+    "Modern American": ["American"],
+    "Traditional American": ["American"],
+    "Steakhouse": ["American"],
+    "Burgers": ["American"],
+    "Diners": ["American"],
+    "Fast Food": ["American"],
+    "Hot Dogs": ["American"],
+    "Seafood": ["American"],
+    "American (Traditional)": ["American"],
 
-yelpset = []
-fourset = []
-openset = []
+    "Tex-Mex": ["Latin American", "Non-American", "Non-White"],
+    "Cuban": ["Latin American", "Non-American", "Non-White"],
+    "Cajun/Creole": ["Latin American", "Non-American", "Non-White"],
+    "Latin American": ["Latin American", "Non-American", "Non-White"],
+    "Mexican": ["Latin American", "Non-American", "Non-White"],
 
-yelp_reader = csv.reader(open('data/yelp_providence/restaurant_info.txt', 'rb'), delimiter = "|")
-for line in yelp_reader:
-    yelpset += [line[0]]
+    "Mediterranean": ["Mediterranean", "Non-American", "Non-White"],
+    "Moroccan": ["Mediterranean", "Non-American", "Non-White"],
+    "Greek": ["Mediterranean", "Non-American", "Non-White"],
+    "Middle Eastern": ["Mediterranean", "Non-American", "Non-White"],
 
-###YELP
-yelp_reader = csv.reader(open('data/yelp_providence/restaurant_info.txt', 'rb'), delimiter = "|")
-next(yelp_reader, None)
-count = 0
-stars = 0
+    "Desserts": ["Dessert"],
+    "Creperies": ["Dessert"],
+    "Ice Cream & Frozen Yogurt": ["Dessert"],
+    "Bakeries": ["Dessert"],
+    "Bakery": ["Dessert"],
+    "Donuts": ["Dessert"],
 
-for line in yelp_reader: #name|latitude|longitude|stars|review_count|open
-    count += 1
-    stars += float(line[3])
+    "Bagels": ["Brunch"],
 
-ystars = stars/count
+    "Food Stands": ["Food Trucks"],
+    "Food Trucks": ["Food Trucks"],
+    "Market Stall": ["Food Trucks"],
 
-print "YELP: " + str(round(ystars, 2))
+    "Ethiopian": ["Non-American", "Non-White"],
 
-###FOURSQUARE
-fs_reader = csv.reader(open('data/foursquare/foursquare.csv', 'rb'), delimiter = "|")
-next(fs_reader, None)
-count = 0
-stars = 0
+    "Pizza": ["Pizza"],
+    "Sandwiches": ["Sandwiches"],
+    "Delis": ["Sandwiches"]
+}
 
-for line in fs_reader: #name|price|score|votes
-    count += 1
-    stars += float(line[2])
+#Score calculations: Multiply by number of reviewers. Ycount is a count of all Yelp reviewers
 
-fstars = (stars/count)/2
+###YELP AND FOURSQUARE
+scores = {} #Name of genre, [yscore, ycount, fscore, fcount]
 
-print "FOURSQUARE: " + str(round(fstars, 2))
+yfs_reader = csv.reader(open('../data/name_matches/yelp_foursquare.csv', 'rb'), delimiter = "|")
+next(yfs_reader, None)
 
-###TRIPADVISOR
+ytotal = 0
+ytotalcount = 0
+ftotal = 0
+ftotalcount = 0
 
-###OPENTABLE
-count = 0
-stars = 0
+for line in yfs_reader: #Name|Price Average|Genres|Yelp Score|Yelp Count|FS Score|FS Count
+    genres = line[2].split(" /// ")
+    if genres[0] == "N/A":
+        continue
+    ycount = int(line[4])
+    yscore = float(line[3])*ycount
+    fcount = int(line[6])
+    fscore = float(line[5])*fcount
 
-opnames = csv.reader(open('data/opentable_names.txt', 'rb'), delimiter = "|")
-for line in opnames:
-    count += 1
-    filename = line[0] + ".csv"
-    stars += otread(filename)
+    ytotal += yscore
+    ytotalcount += ycount
+    ftotal += fscore
+    ftotalcount += fcount
 
-ostars = (stars/count)
+    for genre in genres:
+        if genre not in categories:
+            continue
+        else:
+            maps = categories[genre]
+            for category in maps:
+                if category in scores:
+                    scores[category][0] += yscore
+                    scores[category][1] += ycount
+                    scores[category][2] += fscore
+                    scores[category][3] += fcount
+                else:
+                    scores[category] = [yscore, ycount, fscore, fcount]
 
-print "OPENTABLE: " + str(round(ostars, 2))
+#Total bias throughout the website
+naturalbias = round(ytotal/ytotalcount - ftotal/ftotalcount, 2)
+
+with(open('../data/bias_data/yelp_fs_bias.csv', 'wb')) as r:
+    rwriter = csv.writer(r, delimiter = "|")
+    rwriter.writerow(["Genre", "Yelp", "Foursquare", "Adjusted Bias", "Overall Bias"])
+
+    for genre in scores:
+        info = scores[genre]
+        yelp = info[0]/info[1]
+        foursquare = info[2]/info[3]
+        bias = round(yelp-foursquare, 2)
+        rwriter.writerow([genre, round(yelp, 2), round(foursquare, 2), bias, naturalbias])
 
 
-#######FILTERED BY DOLLAR SIGN
 
-#######FILTERED BY GENRE
+###YELP AND OPENTABLE
+
+
+    
+            
+    
 
 
