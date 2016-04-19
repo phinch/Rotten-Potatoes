4c8dbbc2db69c1174d7464e83692c8bc28c89749
Philip Hinch <phinch@brown.edu>
2016-04-19 02:00:07 -0400

    Yelp Opentable Genre Bias
    Lol there are errors probably

--- Philip's WIP Stuff/genre_bias.py
+++ Philip's WIP Stuff/genre_bias.py
@@ -137,7 +137,7 @@ naturalbias = round(ytotal/ytotalcount - ftotal/ftotalcount, 2)
 
 with(open('../data/bias_data/yelp_fs_bias.csv', 'wb')) as r:
     rwriter = csv.writer(r, delimiter = "|")
-    rwriter.writerow(["Genre", "Yelp", "Foursquare", "Adjusted Bias", "Overall Bias"])
+    rwriter.writerow(["Genre", "Yelp", "Foursquare", "Adjusted Bias", "Site Bias"])
 
     for genre in scores:
         info = scores[genre]
@@ -149,10 +149,96 @@ with(open('../data/bias_data/yelp_fs_bias.csv', 'wb')) as r:
 
 
 ###YELP AND OPENTABLE
+yelpdata = {}
+otdata = {}
+scores = {}
 
+ydata_reader = csv.reader(open('../data/yelp_providence/yelp_pg.csv', 'rb'), delimiter = "|")
+next(ydata_reader, None)
+
+for line in ydata_reader: #Name|Price|Genres|Rating|Review Count|Address
+    name = line[0]
+    genres = line[2]
+    rating = line[3]
+    count = line[4]
+    yelpdata[name] = [genres, rating, count]
+
+ot_reader = csv.reader(open('../data/opentable_pg/opentable_pg.csv', 'rb'), delimiter = "|")
+next(ot_reader, None)
+
+for line in ot_reader: #Name|Price|Genre|Rating|Review Count
+    name = line[0]
+    rating = line[3]
+    count = line[4]
+    otdata[name] = [rating, count]
+
+ynames_reader = csv.reader(open('../yelp_matches_with_opentable.txt', 'rb'))
+otnames_reader = csv.reader(open('../opentable_matches_with_yelp.txt', 'rb'))
+
+yname = ynames_reader.next()
+otname = otnames_reader.next()
+
+ytotal = 0
+ytotalcount = 0
+ottotal = 0
+ottotalcount = 0
+
+while yname[0] != "Paragon": #TODO
+    if (otname[0] not in otdata) or (yname[0] not in yelpdata):
+        print yname, otname
+        yname = ynames_reader.next()
+        otname = otnames_reader.next()
+        continue
+        
+    opentable = otdata[otname[0]]
+    yelp = yelpdata[yname[0]]
+
+    genres = yelp[0].split(" /// ")
+    if genres[0] == "N/A":
+        yname = ynames_reader.next()
+        otname = otnames_reader.next()
+        continue
+
+    ycount = int(yelp[2])
+    yscore = float(yelp[1])*ycount
+
+    otcount = int(opentable[1])
+    otscore = float(opentable[0])*otcount
+
+    ytotal += yscore
+    ytotalcount += ycount
+    ottotal = otscore
+    ottotalcount = otcount
+
+    for genre in genres:
+        if genre not in categories:
+            continue
+        else:
+            maps = categories[genre]
+            for category in maps:
+                if category in scores:
+                    scores[category][0] += yscore
+                    scores[category][1] += ycount
+                    scores[category][2] += otscore
+                    scores[category][3] += otcount
+                else:
+                    scores[category] = [yscore, ycount, otscore, otcount]
+
+    yname = ynames_reader.next()
+    otname = otnames_reader.next()
+
+naturalbias = round(ytotal/ytotalcount - ottotal/ottotalcount, 2)
+
+with(open('../data/bias_data/yelp_ot_bias.csv', 'wb')) as r:
+    rwriter = csv.writer(r, delimiter = "|")
+    rwriter.writerow(["Genre", "Yelp", "Opentable", "Adjusted Bias", "Site Bias"])
+
+    for genre in scores:
+        info = scores[genre]
+        yelp = info[0]/info[1]
+        opentable = info[2]/info[3]
+        bias = round(yelp-opentable, 2)
+        rwriter.writerow([genre, round(yelp, 2), round(opentable, 2), bias, naturalbias])
 
     
-            
-    
-
 
