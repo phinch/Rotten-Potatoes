f675370d595e4a166255d437454d8bb3d95ad01f
Philip Hinch <phinch@brown.edu>
2016-04-23 12:26:35 -0400

    Business Cleaner 2
    New cleaned data; includes review score calculated from actual reviews
    (rather than rounding score given)

--- yelp_dataset/review_cleaner.py
+++ yelp_dataset/review_cleaner.py
@@ -17,19 +17,20 @@ with open('yelp_dataset_challenge_academic_dataset~/yelp_academic_dataset_review
 bids = {}
 
 with open('bids.txt', 'r') as b:
-	for bid in b:
-		stripped = bid.rstrip('\n')
-		bids[stripped] = ""
+    for bid in b:
+        stripped = bid.rstrip('\n')
+        bids[stripped] = ""
 
 with open("review.txt", "wb") as r:
-	r.write("business_id|stars|date\n")
-	for elt in data:
-		business_id = elt['business_id'].encode("utf-8")
-		stars = elt['stars']
-		date = elt['date']
-
+    r.write("business_id|stars|date\n")
+    for elt in data:
+        business_id = elt['business_id'].encode("utf-8")
+        stars = elt['stars']
+        date = elt['date']
+        """
+        I'm just taking this part out for now to hopefully fix KeyErrors in business_cleaner_2 --Philip
 		if business_id not in bids:
 			continue
-
-		toAppend = business_id + "|" + str(stars) + "|" + date + "\n"
-		r.write(toAppend)
+        """
+        toAppend = business_id + "|" + str(stars) + "|" + date + "\n"
+        r.write(toAppend)
