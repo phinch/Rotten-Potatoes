21a6f965d7ba3bd33425ace3b1cb4a78f32855aa
Gary Chien <Gary Chien>
2016-04-30 21:42:31 -0400

    blurg

--- yelp_dataset/scratch.py
+++ yelp_dataset/scratch.py
@@ -1,9 +1,34 @@
 import csv
+from sklearn import tree
 
-with open('attributes.txt', 'rb') as fid:
-    reader = csv.reader(fid,delimiter='|')
+#X = [[0,0,1],[0,1,0],[1,0,0],[1,0,0]]
+#Y = [0,1,2,4]
+#clf = tree.DecisionTreeClassifier()
+#clf = clf.fit(X,Y)
+#print clf.predict_proba([[1,1,0]])
+
+with open('attributes_one-two.txt', 'rb') as fid:
+    reader = csv.reader(fid, delimiter='|')
     labels = reader.next()
-    firstrow = reader.next()
-    print 'num labels:', len(labels)
-    print 'num data:', len(firstrow)
-    assert len(labels) == len(firstrow)
+    good_inds = [1,2,3,4,5,6,12,13,14,19,22,25,32,36]
+    predict_ind = 13
+    X = []
+    Y = []
+    stars = set()
+    noise = set()
+    price = set()
+    attire = set()
+    alcohol = set()
+    for row in reader:
+        feat = []
+        for i in good_inds:
+            curr_val = row[i]
+            if i==6:
+				if curr_val in ['loud', 'very_loud']
+        X.append(feat)
+        Y.append(row[predict_ind])
+    print 'stars:', list(stars)
+    print 'noise:', list(noise)
+    print 'price:', list(price)
+    print 'attire:', list(attire)
+    print 'alcohol:', list(alcohol)
