a4282b0f569de034469143786d23fcce7d4fb39e
Gary Chien <garychien1@gmail.com>
2016-05-01 13:15:08 -0400

    refactored

--- yelp_dataset/DT_undersampled.py
+++ yelp_dataset/DT_undersampled.py
@@ -8,8 +8,8 @@ from sklearn import cross_validation
 def extractData(datapath):
 	with open(datapath, 'rb') as fid:
 		reader = csv.reader(fid, delimiter='|')
-		labels = reader.next()
-		good_inds = [1,2,3,4,5,6,12,14,19,22,25,32,36]
+		reader.next()  # skip past first line
+		good_inds = [1, 2, 3, 4, 5, 6, 12, 14, 19, 22, 25, 32, 36]
 		price_ind = 13
 		cheapX = []
 		cheapY = []
@@ -21,22 +21,22 @@ def extractData(datapath):
 			feat = []
 			for i in good_inds:
 				curr_val = row[i]
-				if i==1: # stars
+				if i == 1:  # stars
 					if curr_val == 'n/a':
-						curr_val = 3.0 # average stars
+						curr_val = 3.0  # average stars
 					else:
 						curr_val = float(curr_val)
-				elif i==6: # noise
+				elif i == 6:  # noise
 					if curr_val in ['loud', 'very_loud']:
 						curr_val = 1
 					else:
 						curr_val = 0
-				elif i==14: # attire
+				elif i == 14:  # attire
 					if curr_val in ['formal', 'dressy']:
 						curr_val = 1
 					else:
 						curr_val = 0
-				elif i==36: # alcohol
+				elif i == 36:  # alcohol
 					if curr_val in ['beer_and_wine', 'full_bar']:
 						curr_val = 1
 					else:
@@ -60,36 +60,36 @@ def extractData(datapath):
 	assert len(cheapX) == len(cheapY)
 	assert len(expX) == len(expY)
 	return cheapX, cheapY, expX, expY
-		
+
 def shuffleListPairs(X, Y):
 	XY = zip(X, Y)
 	random.shuffle(XY)
 	return extractXY(XY)
-	
+
 def extractXY(XY):
-	X = [i for i,j in XY]
-	Y = [j for i,j in XY]
+	X = [i for i, j in XY]
+	Y = [j for i, j in XY]
 	return X, Y
-	
+
 def getUndersampledSets(smallX, smallY, bigX, bigY):
 	bigTrainXY = random.sample(zip(bigX, bigY), len(smallY))
 	bigTrainX, bigTrainY = extractXY(bigTrainXY)
 	X = bigTrainX + smallX
 	Y = bigTrainY + smallY
 	return shuffleListPairs(X, Y)
-	
+
 def classifyWithUndersampling(clf, smallX, smallY, bigX, bigY):
 	len_small = len(smallY)
 	len_big = len(bigY)
 	iter_weight = 5
-	num_iter = int(float(len_big)/len_small)*iter_weight # scale num_iter with big/small ratio
+	num_iter = int(float(len_big) / len_small) * iter_weight  # scale num_iter with big/small ratio
 	accuracies = []
 	# Undersample and obtain average score num_iter times, and then average together the average scores
 	print 'Undersampling... (will sample/train/test %d cheap and %d expensive %d times)' % (len_small, len_small, num_iter)
 	sys.stdout.flush()
 	for _ in range(num_iter):
 		X, Y = getUndersampledSets(smallX, smallY, bigX, bigY)
-		scores = cross_validation.cross_val_score(clf,X,Y,cv=10)
+		scores = cross_validation.cross_val_score(clf, X, Y, cv=10)
 		accuracies.append(scores.mean())
 	accuracies = np.array(accuracies)
 	print 'Finished!'
@@ -99,17 +99,16 @@ def generateDotFileWithUndersampling(clf, smallX, smallY, bigX, bigY):
 	X, Y = getUndersampledSets(smallX, smallY, bigX, bigY)
 	clf = clf.fit(X, Y)
 	with open('DT_undersampled.dot', 'w') as f:
-		f = tree.export_graphviz(clf, out_file=f)
+		tree.export_graphviz(clf, out_file=f)
 	print 'Generated dot file'
-	
+
 def main():
 	cheapX, cheapY, expX, expY = extractData('attributes_all.txt')
 	print 'Num cheap restaurants:', len(cheapY)
 	print 'Num expensive restaurants:', len(expY)
-	clf = tree.DecisionTreeClassifier(max_depth = 3)
+	clf = tree.DecisionTreeClassifier(max_depth=3)
 	classifyWithUndersampling(clf, expX, expY, cheapX, cheapY)
 	generateDotFileWithUndersampling(clf, expX, expY, cheapX, cheapY)
 
-		
 if __name__ == '__main__':
 	main()
